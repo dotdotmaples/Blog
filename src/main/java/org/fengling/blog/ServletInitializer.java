60346c29fc277d9b2e40fe0a50ce392086e6873b
dotdotmaples <xiaohuikj@126.com>
2019-11-07 17:48:03 +0800

    Update ServletInitializer.java
    添加注解

--- src/main/java/org/fengling/blog/ServletInitializer.java
+++ src/main/java/org/fengling/blog/ServletInitializer.java
@@ -3,6 +3,9 @@ package org.fengling.blog;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author 枫铃也
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
